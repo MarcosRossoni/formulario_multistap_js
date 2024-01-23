65e53a5fc817fcfd8246dea2e29ccbddbd40fad6
marcos <marcosrossoni9@gmail.com>
2024-01-23 11:54:41 -0300

    conclusao do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {GrFormNext, GrFormPrevious} from "react-icons/gr";
+import UserForm from "./components/UserForm.jsx";
+import ReviewForm from "./components/ReviewForm.jsx";
+import Thanks from "./components/Thanks.jsx";
+import {useForm} from "./hooks/useForm.jsx";
+import {FiSend} from "react-icons/fi";
+import Steps from "./components/Steps.jsx";
+import {useState} from "react";
+
+const formTemplate = {
+    name: "",
+    email: "",
+    review: "",
+    comment: ""
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [data, setData] = useState(formTemplate)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const updateFielHandler = (key, value) => {
+        setData((prev) => {
+            return {...prev, [key]: value}
+        })
+    }
+
+    const formComponents = [
+        <UserForm data={data} updateFielHandler={updateFielHandler}/>,
+        <ReviewForm data={data} updateFielHandler={updateFielHandler}/>,
+        <Thanks data={data}/>
+    ]
+
+    const {currentStep, currentComponent, changeStep, isLastStep, isFirstStep} = useForm(formComponents)
+
+    return (
+        <div className="app">
+            <div className="header">
+                <h2>Deixe sua avaliação</h2>
+                <p>
+                    Ficamos felizes com a sua compra, utilize o formulario abaixo para avaliar o produto
+                </p>
+            </div>
+            <div className="form-container">
+                <Steps currentStep={currentStep}/>
+                <form onSubmit={(e) => changeStep((currentStep + 1), e)}>
+                    <div className="imputs-container">{currentComponent}</div>
+                    <div className="actions">
+                        {!isFirstStep && (
+                            <button type="button" onClick={() => changeStep(currentStep - 1)}>
+                                <GrFormPrevious/>
+                                <span>Voltar</span>
+                            </button>
+                        )}
+                        {!isLastStep ? (
+                            <button type="submit">
+                                <span>Avançar</span>
+                                <GrFormNext/>
+                            </button>
+                        ) : (
+                            <button type="button">
+                                <span>Enviar</span>
+                                <FiSend />
+                            </button>
+                        )}
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    )
 }
 
 export default App
